776030418eb3a1275e706bda181b2b81fc98a415
anvinps <anvinssb@gmail.com>
2025-07-31 15:11:57 +0530

    suspense added, loader component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/app.scss";
-import { lazy } from "react";
-import Home from "./pages/Home";
+import { lazy, Suspense } from "react";
+import Loader from "./component/Loader";
+const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
