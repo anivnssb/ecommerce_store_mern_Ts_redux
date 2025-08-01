b9fefa383e14b7c4df775af67d11573fd02b9d82
anvinps <anvinssb@gmail.com>
2025-08-01 13:07:53 +0530

    Sidebar seperate li component and type created for navigation

--- src/Admin/components/Sidebar.tsx
+++ src/Admin/components/Sidebar.tsx
@@ -1,7 +1,8 @@
+import type { IconType } from "react-icons";
 import { AiFillFileText } from "react-icons/ai";
 import { IoIosPeople } from "react-icons/io";
 import { RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, type Location } from "react-router-dom";
 
 const Sidebar = () => {
   const location = useLocation();
@@ -11,62 +12,83 @@ const Sidebar = () => {
       <div>
         <h5>Dashboard</h5>
         <ul>
-          <li
-            style={{
-              backgroundColor:
-                location.pathname === "/admin/dashboard"
-                  ? "rgb(0,115,255,0.1)"
-                  : "white",
-            }}
-          >
-            <Link to="/admin/dashboard">
-              <RiDashboardFill />
-              Dashboard
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor:
-                location.pathname === "/admin/products"
-                  ? "rgb(0,115,255,0.1)"
-                  : "white",
-            }}
-          >
-            <Link to="/admin/products">
-              <RiShoppingBag3Fill />
-              Products
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor:
-                location.pathname === "/admin/customers"
-                  ? "rgb(0,115,255,0.1)"
-                  : "white",
-            }}
-          >
-            <Link to="/admin/customers">
-              <IoIosPeople />
-              Customers
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor:
-                location.pathname === "/admin/transactions"
-                  ? "rgb(0,115,255,0.1)"
-                  : "white",
-            }}
-          >
-            <Link to="/admin/transactions">
-              <AiFillFileText />
-              Transacions
-            </Link>
-          </li>
+          <Li
+            url="/admin/dashboard"
+            location={location}
+            Icon={RiDashboardFill}
+            text="Dashboard"
+          />
+          <Li
+            url="/admin/products"
+            location={location}
+            Icon={RiShoppingBag3Fill}
+            text="Products"
+          />
+          <Li
+            url="/admin/customers"
+            location={location}
+            Icon={IoIosPeople}
+            text="Customers"
+          />
+          <Li
+            url="/admin/transactions"
+            location={location}
+            Icon={AiFillFileText}
+            text="Transacions"
+          />
+        </ul>
+      </div>
+      <div>
+        <h5>Charts</h5>
+        <ul>
+          <Li
+            url="/admin/dashboard"
+            location={location}
+            Icon={RiDashboardFill}
+            text="Dashboard"
+          />
+          <Li
+            url="/admin/products"
+            location={location}
+            Icon={RiShoppingBag3Fill}
+            text="Products"
+          />
+          <Li
+            url="/admin/customers"
+            location={location}
+            Icon={IoIosPeople}
+            text="Customers"
+          />
+          <Li
+            url="/admin/transactions"
+            location={location}
+            Icon={AiFillFileText}
+            text="Transacions"
+          />
         </ul>
       </div>
     </aside>
   );
 };
 
+interface LiProps {
+  url: string;
+  location: Location;
+  text: string;
+  Icon: IconType;
+}
+
+const Li = ({ url, location, text, Icon }: LiProps) => (
+  <li
+    style={{
+      backgroundColor:
+        location.pathname === url ? "rgb(0,115,255,0.1)" : "white",
+    }}
+  >
+    <Link to={url}>
+      <Icon />
+      {text}
+    </Link>
+  </li>
+);
 export default Sidebar;
