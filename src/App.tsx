2fe09becefaf64989e23a4d767697803193b471f
anvinps <anvinssb@gmail.com>
2025-08-01 10:19:15 +0530

    admin admin pages and stylesheet created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/app.scss";
 import { lazy, Suspense } from "react";
-import Loader from "./component/Loader";
+import Loader from "./components/Loader";
+const DashBoard = lazy(() => import("./pages/Admin/DashBoard"));
+const Products = lazy(() => import("./pages/Admin/Products"));
+const Customers = lazy(() => import("./pages/Admin/Customers"));
+const Transactions = lazy(() => import("./pages/Admin/Transactions"));
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
@@ -14,6 +18,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+
+          <Route path="/admin/dashboard" element={<DashBoard />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/customers" element={<Customers />} />
+          <Route path="/admin/transactions" element={<Transactions />} />
         </Routes>
       </Suspense>
     </Router>
