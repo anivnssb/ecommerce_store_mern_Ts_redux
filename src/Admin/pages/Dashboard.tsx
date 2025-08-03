db5290e461d13f19cc8fca2c1bf274deacec37bb
anvinps <anvinssb@gmail.com>
2025-08-03 18:51:54 +0530

    dashboard search input changes

--- src/Admin/pages/Dashboard.tsx
+++ src/Admin/pages/Dashboard.tsx
@@ -1,11 +1,19 @@
+import { BsSearch } from "react-icons/bs";
 import Sidebar from "../components/Sidebar";
+import { FaRegBell } from "react-icons/fa";
+import userImage from "../../assets/images/userpic.png";
 
 const Dashboard = () => {
   return (
     <div className="adminContainer">
       <Sidebar />
       <main className="dashboard">
-        <div className="bar">asd</div>
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data, users, logs, docs" />
+          <FaRegBell />
+          <img src={userImage} alt="User" />
+        </div>
       </main>
     </div>
   );
