538be10736dab7f7d6956745913f8437019509c4
anvinps <anvinssb@gmail.com>
2025-08-05 22:06:45 +0530

    dashboard widget item component

--- src/Admin/pages/Dashboard.tsx
+++ src/Admin/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { BsSearch } from "react-icons/bs";
 import Sidebar from "../components/Sidebar";
 import { FaRegBell } from "react-icons/fa";
 import userImage from "../../assets/images/userpic.png";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 
 const Dashboard = () => {
   return (
@@ -14,9 +15,69 @@ const Dashboard = () => {
           <FaRegBell />
           <img src={userImage} alt="User" />
         </div>
+        <section className="widgetcontainer">
+          <WidgetItem
+            {...{
+              heading: "Revenue",
+              value: 12000,
+              percent: 15,
+              color: "rgb(0,115,255)",
+              amount: true,
+            }}
+          />
+          <WidgetItem
+            {...{
+              heading: "Revenue",
+              value: 12000,
+              percent: 15,
+              color: "rgb(0,115,255)",
+              amount: true,
+            }}
+          />
+          <WidgetItem
+            {...{
+              heading: "Revenue",
+              value: 12000,
+              percent: 15,
+              color: "rgb(0,115,255)",
+              amount: true,
+            }}
+          />
+        </section>
       </main>
     </div>
   );
 };
+
+interface widgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
+}
+const WidgetItem = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: widgetItemProps) => (
+  <article className="widget">
+    <div className="widgetInfo">
+      <p>{heading}</p>
+      <h4>{amount ? `$${value}` : value}</h4>
+      {percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp /> +{percent} "%"
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown /> +{percent} + "%"
+        </span>
+      )}
+    </div>
+  </article>
+);
 
 export default Dashboard;
