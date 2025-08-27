459fc1c81e901d2442bc6f4b611f64c73c78ff9d
anvinssb <anvinssb@gmail.com>
2025-08-27 13:01:14 +0530

    db connection,error middleware, error handling

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,9 +1,21 @@
 import express from "express";
 
+import userRoute from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleWare } from "./middlewares/error.js";
 const port = 3000;
 
+connectDB();
 const app = express();
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Api working with /api/v1");
+});
+// using routes
+app.use("/api/v1/user", userRoute);
+
+app.use(errorMiddleWare);
 app.listen(port, () => {
   console.log(`server is working on localhost:${port}`);
 });
